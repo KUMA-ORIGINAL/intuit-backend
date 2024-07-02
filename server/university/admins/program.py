2c4fe864248ea85e8a82fb3fcad76a97f0e10239
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2024-07-02 17:14:39 +0600

    add more futures for program

--- server/university/admins/program.py
+++ server/university/admins/program.py
@@ -1,19 +1,74 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from university.models.program import Program
+from university.models.program import Program, TrainingProgram, TrainingProgramItem, ProgramSkills, \
+    ProgramTools
+
+
+class TrainingProgramItemInline(admin.TabularInline):
+    model = TrainingProgramItem
+    extra = 1
+
+
+class ProgramSkillsInline(admin.TabularInline):
+    model = ProgramSkills.program.through
+    extra = 1
+
+
+class ProgramToolsInline(admin.TabularInline):
+    model = ProgramTools.program.through
+    extra = 1
 
 
 @admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
-    list_display = ['title', "get_photo"]
-    search_fields = ["title"]
-    list_filter = ["title"]
-    prepopulated_fields = {"slug": ["title"]}
+    list_display = ['title', 'get_photo']
+    search_fields = ['title']
+    list_filter = ['title']
+    prepopulated_fields = {'slug': ['title']}
     list_per_page = 20
-    readonly_fields = ["get_photo"]
-    filter_horizontal = [ "faculty", 'education_level']
+    readonly_fields = ['get_photo']
+    filter_horizontal = ['faculty', 'education_level']
+    inlines = [ProgramSkillsInline, ProgramToolsInline]
 
-    def get_photo(self, object):
-        if object.photo:
-            return mark_safe(f"<img src='{object.photo.url}' height=80>")
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f"<img src='{obj.photo.url}' height=80>")
+        return ''
+    get_photo.short_description = 'Фото'
+
+
+@admin.register(TrainingProgram)
+class TrainingProgramAdmin(admin.ModelAdmin):
+    inlines = [TrainingProgramItemInline]
+    list_display = ['number', 'program']
+    list_filter = ['program']
+    search_fields = ['number']
+
+@admin.register(ProgramSkills)
+class ProgramSkillsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'get_programs']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+    filter_horizontal = ('program',)
+
+    def get_programs(self, obj):
+        return ", ".join([p.title for p in obj.program.all()])
+    get_programs.short_description = 'Программы'
+
+@admin.register(ProgramTools)
+class ProgramToolsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'get_logo', 'get_programs']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+    filter_horizontal = ('program',)
+
+    def get_programs(self, obj):
+        return ", ".join([p.title for p in obj.program.all()])
+    get_programs.short_description = 'Программы'
+
+    def get_logo(self, obj):
+        if obj.logo:
+            return mark_safe(f"<img src='{obj.logo.url}' height=80>")
+        return ''
+    get_logo.short_description = 'Лого инструмента'
--- server/university/serializers/program.py
+++ server/university/serializers/program.py
@@ -1,9 +1,38 @@
 from rest_framework import serializers
 
-from university.models.program import Program
+from university.models.program import (Program, TrainingProgram, TrainingProgramItem,
+                                       ProgramTools, ProgramSkills)
+
+class TrainingProgramItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrainingProgramItem
+        fields = ('name',)
+
+
+class TrainingProgramSerializer(serializers.ModelSerializer):
+    items = TrainingProgramItemSerializer(many=True, read_only=True)
+    class Meta:
+        model = TrainingProgram
+        fields = ('number', 'items')
+
+
+class ProgramToolsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProgramTools
+        fields = ('name', 'logo')
+
+
+class ProgramSkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProgramSkills
+        fields = ('name',)
 
 
 class ProgramSerializer(serializers.ModelSerializer):
+    training_programs = TrainingProgramSerializer(many=True, read_only=True)
+    skills = ProgramSkillsSerializer(many=True, read_only=True)
+    tools = ProgramToolsSerializer(many=True, read_only=True)
+
     class Meta:
         model = Program
         fields = '__all__'

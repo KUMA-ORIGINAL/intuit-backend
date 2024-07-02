8b15e19751947b4a11e4132695b8b729b1ed05ca
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2024-07-02 13:30:55 +0600

    refact EducationLevelSerializer

--- server/university/serializers/education_level.py
+++ server/university/serializers/education_level.py
@@ -4,6 +4,9 @@ from university.models.education_level import EducationLevel
 
 
 class EducationLevelSerializer(serializers.ModelSerializer):
+    admission_eligibility = serializers.SlugRelatedField(many=True,
+                                                         read_only=True,
+                                                         slug_field='name')
 
     class Meta:
         model = EducationLevel

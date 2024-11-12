6c333c103ef549838010b4b052bdd0a2a843bd0b
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2024-11-12 16:19:41 +0600

    refact admin modeltransaltion ui

--- server/university/admins/faculty.py
+++ server/university/admins/faculty.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from modeltranslation.admin import TranslationAdmin
+from modeltranslation.admin import TranslationAdmin, TabbedTranslationAdmin
 
 from university.models.faculty import Faculty
 
 
 @admin.register(Faculty)
-class FacultyAdmin(TranslationAdmin):
+class FacultyAdmin(TabbedTranslationAdmin):
     list_display = ['id', 'title', "get_photo"]
     list_display_links = ['id', 'title',]
     search_fields = ["title"]
@@ -17,13 +17,3 @@ class FacultyAdmin(TranslationAdmin):
     def get_photo(self, object):
         if object.banner:
             return mark_safe(f"<img src='{object.banner.url}' height=80>")
-
-    class Media:
-        js = (
-            'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields.js',
-        )
-        css = {
-            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-        }

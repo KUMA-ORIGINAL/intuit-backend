c7330c21c3672dc34c69b1b01412146271cca3c3
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2024-12-09 14:02:22 +0600

    add qr_codes for staffs

--- server/university/serializers/staff.py
+++ server/university/serializers/staff.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 
-from university.models.staff import Staff
+from university.models import Faculty
+from university.models.staff import Staff, Position
+
+
+class PositionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Position
+        fields = ('id', 'title')
 
 
 class StaffSerializer(serializers.ModelSerializer):
+    position = PositionSerializer(many=True)
 
     class Meta:
         model = Staff
-        fields = '__all__'
+        fields = ('id', 'name',  'slug', 'position', 'rank', 'status', 'description',
+                  'image', 'whatsapp', 'facebook', 'telegram', 'instagram', 'youtube',
+                  'curriculum_vitae', 'cv', 'qr_code', 'faculty')

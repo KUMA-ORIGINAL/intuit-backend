dd9812bd77ab85d69ddff726d81b046a55c0fb13
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2024-12-31 01:41:15 +0600

    refact views.py news

--- server/news/views.py
+++ server/news/views.py
@@ -7,7 +7,7 @@ from .models import Post
 from .serializers import PostSerializer
 
 class PostPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 30
     page_size_query_param = 'page_size'
     max_page_size = 100
 
